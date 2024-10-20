c5ea13bcdbef9632ee83ce02f4e3eadf06d393ea
Radosław Kiczek <radekkiczekk@gmail.com>
2024-10-20 21:16:04 +0200

    Implemented TokenService

--- WorkSchedule.Api/Program.cs
+++ WorkSchedule.Api/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using MySqlConnector;
+using WorkSchedule.Infrastructure;
 using WorkSchedule.Infrastructure.Data;
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,8 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<WorkScheduleContext>(opt =>
     opt.UseMySql(connectionString,
     ServerVersion.AutoDetect(connectionString)));
+
+builder.Services.AddInfrastructure();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ WorkSchedule.Infrastructure/Extension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkSchedule.Application.Interfaces;
+using WorkSchedule.Infrastructure.Token;
+
+namespace WorkSchedule.Infrastructure;
+
+public static class Extension
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddTransient<ITokenService, TokenService>();
+        return services;
+    }
+}
