50f52b8e09eb43e15714c5c1a03bd7e9003c3c00
kmlwoj <wujekkama@gmail.com>
2024-10-27 21:33:37 +0100

    Added DTOs for roles, preferences and workdays

---
+++ WorkSchedule.Api/Models/WorkDayBaseModel.cs
@@ -0,0 +1,25 @@
+﻿namespace WorkSchedule.Api.Models
+{
+    public class WorkDayBaseModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public WorkDayBaseModel()
+        {
+            Name = string.Empty;
+            Surname = string.Empty;
+            Email = string.Empty;
+        }
+        public WorkDayBaseModel(string name, string surname, string email, DateTime start, DateTime end)
+        {
+            Name = name;
+            Surname = surname;
+            Email = email;
+            Start = start;
+            End = end;
+        }
+    }
+}
---
+++ WorkSchedule.Api/Models/WorkDayModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WorkSchedule.Api.Models
+{
+    public class WorkDayModel : WorkDayBaseModel
+    {
+        public WorkDayModel() : base() { }
+        public WorkDayModel(string name, string surname, string email, DateTime start, DateTime end) : base(name, surname, email, start, end) { }
+    }
+}
