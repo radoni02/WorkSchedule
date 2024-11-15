83ed4e18d6e01cfa6c8d6d3befe058d3e75d67ed
kmlwoj <wujekkama@gmail.com>
2024-11-15 18:51:47 +0100

    Addition of preferences controller

---
+++ WorkSchedule.Api/Controllers/PreferencesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WorkSchedule.Application.Interfaces;
+using WorkSchedule.Infrastructure.Token;
+
+namespace WorkSchedule.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PreferencesController : ControllerBase
+    {
+        private readonly IUserRepository _userRepo;
+        private readonly ITokenService _tokenService;
+        private readonly IPasswordHasher _passwordHasher;
+        private readonly LoginUserService _loginUserService;
+
+        public PreferencesController(IUserRepository userRepo, ITokenService tokenService, IPasswordHasher passwordHasher, LoginUserService loginUserService)
+        {
+            _userRepo = userRepo;
+            _tokenService = tokenService;
+            _passwordHasher = passwordHasher;
+            _loginUserService = loginUserService;
+        }
+
+        [HttpGet("GetPreferences")]
+    }
+}
