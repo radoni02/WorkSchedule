b746cc9e56849e6c1fc2ae3a18edda393a94af5e
kmlwoj <wujekkama@gmail.com>
2024-11-16 16:13:44 +0100

    Added xml comments for endpoints and an authorization option for swagger

--- WorkSchedule.Api/Program.cs
+++ WorkSchedule.Api/Program.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using MySqlConnector;
+using System.Reflection;
 using System.Text;
 using WorkSchedule.Infrastructure;
 using WorkSchedule.Infrastructure.Data;
@@ -46,7 +48,40 @@ builder.Services.AddInfrastructure();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1",
+        new OpenApiInfo
+        {
+            Title = "WorkSchedule",
+            Version = "v1"
+        });
+
+    string xmlFile = string.Concat(Assembly.GetExecutingAssembly().GetName().Name, ".xml");
+    string filePath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(filePath);
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme {
+                Reference = new OpenApiReference {
+                    Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 var app = builder.Build();
 
